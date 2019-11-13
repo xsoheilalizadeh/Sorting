ca6429cc5ed95bfe504affc2f62d17e3f0876ad1
Soheil Alizadeh <xsoheilalizadeh@gmail.com>
2019-11-13 20:42:17 +0330

    Initial Commit

---
+++ Sorting/Benchmarks.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace Sorting
+{
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    public class Benchmarks
+    {
+        private readonly int[] data;
+
+        public Benchmarks()
+        {
+            this.data = new[] {1, 2, 5, 1, 7, 2, 4, 2, 9, 7, 11, 28, 14, 1};
+        }
+
+        [Benchmark]
+        public void HashSet()
+        {
+            var numbers = new HashSet<int>(data).ToArray();
+            Array.Sort(numbers);
+        }
+
+        [Benchmark]
+        public void ImmutableSortedSet()
+        {
+            var sortedData = new HashSet<int>(data).ToImmutableSortedSet();
+        }
+
+        [Benchmark]
+        public void SortedSet()
+        {
+            var sortedData = new SortedSet<int>(data);
+        }
+    }
+}
---
+++ Sorting/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Benchmarks>();
+        }
+    }
+}
